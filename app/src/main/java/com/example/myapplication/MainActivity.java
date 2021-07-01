12a688e53385e56b5d7724d83e15cb33df0ebba6
limetree06 <limetree06@naver.com>
2021-07-01 15:00:47 +0900

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+//this is minji's
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
