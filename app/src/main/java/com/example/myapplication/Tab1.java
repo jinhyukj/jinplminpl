49a2fb6b39dbbfd68b197ee1918abe3e7ee434e0
limetree06 <limetree06@naver.com>
2021-07-03 16:37:40 +0900

    7/3 commit

--- app/src/main/java/com/example/myapplication/Tab1.java
+++ app/src/main/java/com/example/myapplication/Tab1.java
@@ -39,7 +39,7 @@ public class Tab1 extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCrateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_tab1, container, false);
         recyclerView = view.findViewById(R.id.recycler_view);
         checkPermisson();
@@ -103,13 +103,10 @@ public class Tab1 extends Fragment {
 
                     //Initialize contact model
                     ContactModel model = new ContactModel();
-
                     //Set name
                     model.setName(name);
-
                     //Set number
                     model.setNumber(number);
-
                     //Add model in array list
                     arrayList.add(model);
 
