6b663b5c51b09b2558d3afd27fe3b30028fdb0fe
limetree06 <limetree06@naver.com>
2021-07-04 23:26:25 +0900

    add contact xml

--- app/src/main/java/com/example/myapplication/addContact.java
+++ app/src/main/java/com/example/myapplication/addContact.java
@@ -1,6 +1,15 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class addContact extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_add);
+
+
+
+    }
 }
