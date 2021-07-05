63ea659a269641c10b604d99f0f4c9960ad07246
limetree06 <limetree06@naver.com>
2021-07-05 22:10:10 +0900

    image layout

--- app/src/main/java/com/example/myapplication/Tab3.java
+++ app/src/main/java/com/example/myapplication/Tab3.java
@@ -56,11 +56,8 @@ import yuku.ambilwarna.AmbilWarnaDialog;
 import static android.app.Activity.RESULT_OK;
 
 
-public class Tab3 extends Fragment implements OnClickListener, OnTouchListener {
-    PaintView paintView;
+public class Tab3 extends Fragment {
     ImageView choosenImageView;
-    Button choosePicture;
-    Button savePicture;
 
     Bitmap bmp;
     Bitmap alteredBitmap;
@@ -68,15 +65,10 @@ public class Tab3 extends Fragment implements OnClickListener, OnTouchListener {
     Paint paint;
     Matrix matrix;
 
-    float downx = 0;
-    float downy = 0;
-    float upx = 0;
-    float upy = 0;
-
     int defaultColor;
 
     SignatureView signatureView;
-    ImageButton imageEraser, imgColor, imgSave;
+    ImageButton imageEraser, imgColor, imgSave, choosePicture;
     SeekBar seekBar;
     TextView txtPenSize;
 
@@ -89,15 +81,9 @@ public class Tab3 extends Fragment implements OnClickListener, OnTouchListener {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_tab3, container, false);
         context = container.getContext();
 
-        paintView = new PaintView(this.getContext());
         //choosenImageView = view.findViewById(R.id.ChoosenImageView);
-        //choosePicture = view.findViewById(R.id.ChoosePictureButton);
-        //savePicture = view.findViewById(R.id.SavePictureButton);
 
-        //savePicture.setOnClickListener(this);
-        //choosePicture.setOnClickListener(this);
-        //choosenImageView.setOnTouchListener(this);
-
+        choosePicture = view.findViewById(R.id.ChoosePictureButton);
         signatureView = view.findViewById(R.id.signature_view);
         imageEraser = view.findViewById(R.id.btnEraser);
         imgColor = view.findViewById(R.id.btnColor);
@@ -109,13 +95,31 @@ public class Tab3 extends Fragment implements OnClickListener, OnTouchListener {
 
         defaultColor = ContextCompat.getColor(getActivity(), R.color.black);
 
-
         imgColor.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 openColorPicker();
             }
         });
+
+        imageEraser.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view1){
+                signatureView.clearCanvas();
+            }
+        });
+
+        choosePicture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view1){
+                //choosenImageView.setOnTouchListener(this);
+                Intent choosePictureIntent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(choosePictureIntent, 0);
+            }
+        });
+
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -134,14 +138,31 @@ public class Tab3 extends Fragment implements OnClickListener, OnTouchListener {
 
         imgSave.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                openColorPicker();
+            public void onClick(View view) {
+                if(!signatureView.isBitmapEmpty()){
+                    SaveImage();
+                }
             }
-
         });
 
 
         return view;
+    }
+
+    private void SaveImage() {
+            ContentValues contentValues = new ContentValues(3);
+            contentValues.put(Media.DISPLAY_NAME, "Draw On Me");
+
+            Uri imageFileUri = getActivity().getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, contentValues);
+            try {
+                OutputStream imageFileOS = getActivity().getContentResolver().openOutputStream(imageFileUri);
+                alteredBitmap.compress(CompressFormat.JPEG, 90, imageFileOS);
+                Toast t = Toast.makeText(context, "Saved!", Toast.LENGTH_SHORT);
+                t.show();
+
+            } catch (Exception e) {
+                Log.v("EXCEPTION", e.getMessage());
+            }
     }
 
     private void openColorPicker() {
@@ -177,51 +198,19 @@ public class Tab3 extends Fragment implements OnClickListener, OnTouchListener {
         }).check();
     }
 
-
-    public void onClick(View v) {
-        if (v == choosePicture) {
-            Intent choosePictureIntent = new Intent(
-                    Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(choosePictureIntent, 0);
-        }
-        else if (v == savePicture) {
-            if (alteredBitmap != null) {
-                ContentValues contentValues = new ContentValues(3);
-                contentValues.put(Media.DISPLAY_NAME, "Draw On Me");
-
-                Uri imageFileUri = getActivity().getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, contentValues);
-                try {
-                    OutputStream imageFileOS = getActivity().getContentResolver().openOutputStream(imageFileUri);
-                    alteredBitmap.compress(CompressFormat.JPEG, 90, imageFileOS);
-                    Toast t = Toast.makeText(context, "Saved!", Toast.LENGTH_SHORT);
-                    t.show();
-
-                } catch (Exception e) {
-                    Log.v("EXCEPTION", e.getMessage());
-                }
-            }
-        }
-    }
-
-    public void onActivityResult(int requestCode, int resultCode,
-                                 Intent intent) {
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-
         if (resultCode == RESULT_OK) {
             Uri imageFileUri = intent.getData();
             try {
                 BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
                 bmpFactoryOptions.inJustDecodeBounds = true;
-                bmp = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(
-                        imageFileUri), null, bmpFactoryOptions);
+                bmp = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(imageFileUri), null, bmpFactoryOptions);
 
                 bmpFactoryOptions.inJustDecodeBounds = false;
-                bmp = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(
-                        imageFileUri), null, bmpFactoryOptions);
+                bmp = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(imageFileUri), null, bmpFactoryOptions);
 
-                alteredBitmap = Bitmap.createBitmap(bmp.getWidth(), bmp
-                        .getHeight(), bmp.getConfig());
+                alteredBitmap = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
 
                 canvas = new Canvas(alteredBitmap);
                 paint = new Paint();
@@ -237,34 +226,7 @@ public class Tab3 extends Fragment implements OnClickListener, OnTouchListener {
             }
         }
     }
-    public boolean onTouch(View v, MotionEvent event) {
-        int action = event.getAction();
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                downx = event.getX();
-                downy = event.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                upx = event.getX();
-                upy = event.getY();
-                canvas.drawLine(downx, downy, upx, upy, paint);
-                choosenImageView.invalidate();
-                downx = upx;
-                downy = upy;
-                break;
-            case MotionEvent.ACTION_UP:
-                upx = event.getX();
-                upy = event.getY();
-                canvas.drawLine(downx, downy, upx, upy, paint);
-                choosenImageView.invalidate();
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                break;
-            default:
-                break;
-        }
-        return true;
-    }
+
 
 
 }
