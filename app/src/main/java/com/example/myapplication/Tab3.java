2fb98362469d89fc933d2738a9b940409a4bbee7
limetree06 <limetree06@naver.com>
2021-07-06 14:39:01 +0900

    !!

--- app/src/main/java/com/example/myapplication/Tab3.java
+++ app/src/main/java/com/example/myapplication/Tab3.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,10 +10,11 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.paint.PaintView;
 import com.karumi.dexter.Dexter;
@@ -20,7 +22,6 @@ import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-import com.kyanogen.signatureview.SignatureView;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -33,10 +34,6 @@ import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -55,26 +52,16 @@ import android.widget.Toast;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
 
-import static android.app.Activity.RESULT_OK;
-
 
 public class Tab3 extends Fragment {
-    ImageView choosenImageView;
-
-    Bitmap bmp;
-    Bitmap alteredBitmap;
-    Canvas canvas;
-    Paint paint;
-    Matrix matrix;
     Switch lock;
 
-    int defaultColor;
+    int defaultColor, penSize;
 
-    SignatureView signatureView;
-    ImageButton imageEraser, imgColor, imgSave, choosePicture;
+    ImageButton imageEraser, imgColor, choosePicture;
     SeekBar seekBar;
     TextView txtPenSize;
-
+    PaintView paintView;
 
     Context context;
 
@@ -84,19 +71,19 @@ public class Tab3 extends Fragment {
         super.onCreate(savedInstanceState);
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_tab3, container, false);
         //context = container.getContext();
-
         //choosenImageView = view.findViewById(R.id.ChoosenImageView);
-
+        paintView = view.findViewById(R.id.paintView);
         choosePicture = view.findViewById(R.id.ChoosePictureButton);
-        signatureView = view.findViewById(R.id.signature_view);
         imageEraser = view.findViewById(R.id.btnEraser);
         imgColor = view.findViewById(R.id.btnColor);
-        imgSave = view.findViewById(R.id.btnSave);
         seekBar = view.findViewById(R.id.penSize);
         txtPenSize = view.findViewById(R.id.txtPenSize);
         lock = view.findViewById(R.id.lock);
 
         askPermission();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        paintView.initialise(displayMetrics);
 
         defaultColor = ContextCompat.getColor(getActivity(), R.color.black);
 
@@ -110,9 +97,25 @@ public class Tab3 extends Fragment {
         imageEraser.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view1){
-                signatureView.clearCanvas();
+                paintView.clear();
             }
         });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress = seekBar.getProgress();
+                paintView.setStrokeWidth(progress);
+                txtPenSize.setText(progress + "dp");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
 
 
         lock.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -124,74 +127,54 @@ public class Tab3 extends Fragment {
         choosePicture.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view1){
-                //choosenImageView.setOnTouchListener(this);
-                Intent choosePictureIntent = new Intent(
-                        Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(choosePictureIntent, 0);
+                // 인텐트 객체 생성
+                Intent intent = new Intent(getContext(), drawOnImage.class);
+                // 새로운 액티비티 시작
+                intent.putExtra("color", defaultColor);
+                intent.putExtra("pen Size", penSize);
+
+                startActivityForResult(intent, 1000);
             }
         });
-
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                txtPenSize.setText(progress + "dp");
-                signatureView.setPenSize(progress);
-                seekBar.setMax(50);
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-
-        imgSave.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                if(!signatureView.isBitmapEmpty()){
-                    SaveImage();
-                }
-            }
-        });
-
 
         return view;
-    }
-
-    private void SaveImage() {
-            ContentValues contentValues = new ContentValues(3);
-            contentValues.put(Media.DISPLAY_NAME, "Draw On Me");
-
-            Uri imageFileUri = getActivity().getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, contentValues);
-            try {
-                OutputStream imageFileOS = getActivity().getContentResolver().openOutputStream(imageFileUri);
-                alteredBitmap.compress(CompressFormat.JPEG, 90, imageFileOS);
-                Toast t = Toast.makeText(getContext(), "Saved!", Toast.LENGTH_SHORT);
-                t.show();
-
-            } catch (Exception e) {
-                Log.v("EXCEPTION", e.getMessage());
-            }
     }
 
     private void openColorPicker() {
         AmbilWarnaDialog ambilWarnaDialog = new AmbilWarnaDialog(getContext(), defaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
             @Override
             public void onCancel(AmbilWarnaDialog dialog) {
-
+                Toast.makeText(getContext(), "Unavailable", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onOk(AmbilWarnaDialog dialog, int color) {
-                defaultColor= color;
-                signatureView.setPenColor(color);
+                defaultColor = color;
+                paintView.setColor(color);
             }
         });
-        ambilWarnaDialog.show();
+        ambilWarnaDialog.show(); // add
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.clear_button:
+                paintView.clear();
+                return true;
+            case R.id.undo_button:
+                paintView.undo();
+                return true;
+            case R.id.redo_button:
+                paintView.redo();
+                return true;
+            case R.id.save_button:
+                paintView.saveImage();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     private void askPermission(){
         Dexter.withContext(getContext())
@@ -209,36 +192,5 @@ public class Tab3 extends Fragment {
             }
         }).check();
     }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        if (resultCode == RESULT_OK) {
-            Uri imageFileUri = intent.getData();
-            try {
-                BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
-                bmpFactoryOptions.inJustDecodeBounds = true;
-                bmp = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(imageFileUri), null, bmpFactoryOptions);
-
-                bmpFactoryOptions.inJustDecodeBounds = false;
-                bmp = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(imageFileUri), null, bmpFactoryOptions);
-
-                alteredBitmap = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
-
-                canvas = new Canvas(alteredBitmap);
-                paint = new Paint();
-                paint.setColor(Color.GREEN);
-                paint.setStrokeWidth(5);
-                matrix = new Matrix();
-                canvas.drawBitmap(bmp, matrix, paint);
-
-                choosenImageView.setImageBitmap(alteredBitmap);
-                choosenImageView.setOnTouchListener((OnTouchListener) this);
-            } catch (Exception e) {
-                Log.v("ERROR", e.toString());
-            }
-        }
-    }
-
-
 
 }
